91430b953bc3a5aefaeb33cacae441447c5a5b59
Hatreds2 <adambattiste2@gmail.com>
2025-10-31 00:05:18 +0000

    Sec 2 ch10
    Work done so far up till ch2 section 10

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+const country = "United Kingdom";
+const continent = "Europe";
+let population = "66";
+
+
+console.log(continent);
+
+const isIsland = "true";
+const language = "English";
+console.log(isIsland);
+console.log(population);
+console.log(language);
+console.log(country);
+
